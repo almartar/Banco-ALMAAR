24c8b091e6010aa834ccfeea990e7c9c35bf6ba4
almartar <94218072+almartar@users.noreply.github.com>
2025-01-12 20:24:31 +0100

    Ajustes

--- app/src/main/java/com/albertomarti/banco_almaar/login/LoginActivity.kt
+++ app/src/main/java/com/albertomarti/banco_almaar/login/LoginActivity.kt
@@ -2,15 +2,14 @@ package com.albertomarti.banco_almaar.login
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.TextView
-import android.widget.Toast
+
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.albertomarti.banco_almaar.AppPersistant
-import com.albertomarti.banco_almaar.FragmentBlue
-import com.albertomarti.banco_almaar.R
+
+
 import com.albertomarti.banco_almaar.databinding.ActivityLoginBinding
-import com.albertomarti.banco_almaar.fragmentRojo
+
 import com.albertomarti.banco_almaar.home.MainActivity
 import com.example.bancoapiprofe.dao.ClienteDAO
 import com.example.bancoapiprofe.pojo.Cliente
@@ -19,6 +18,8 @@ import com.example.bancoapiprofe.pojo.Cliente
 class LoginActivity : AppCompatActivity() {
     lateinit var binding: ActivityLoginBinding
 
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityLoginBinding.inflate(layoutInflater)
@@ -26,7 +27,8 @@ class LoginActivity : AppCompatActivity() {
         setUpView()
         AppPersistant.initialize(this)
 
-        binding.bb.setOnClickListener{
+
+        binding.signInButtonId.setOnClickListener{
             openHome(binding.userEditTextId.text.toString())
         }
     }
@@ -40,19 +42,11 @@ class LoginActivity : AppCompatActivity() {
 
 
 
-    private fun loadFragment(fragment: Fragment) {
-        // Iniciar una transacción de fragment
-        val transaction = supportFragmentManager.beginTransaction()
-        transaction.replace(R.id.fragmentContainer, fragment)
-        transaction.addToBackStack(null) // Agregar a la pila de retroceso
-        transaction.commit()
-    }
 
 
 
 
-    // COPY_ALBERTO
-    // Sobreescribir la función completa en tu código
+
     private fun openHome(dni: String) {
         val intent = Intent(this, MainActivity::class.java)
         intent.putExtra("DNI_k", dni)
